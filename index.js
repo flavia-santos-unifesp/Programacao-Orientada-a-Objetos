4e810233f9e521e3a26b368a24437c9e8c462afd
flavia-santos-unifesp <ffsantos24@unifesp.br>
2026-07-05 18:51:15 -0300

    Melhorar resolução de path no index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-require("./PetSystemAnalytics/backend/dist/src/index.js");
+const path = require("path");
+const filePath = path.join(__dirname, "PetSystemAnalytics/backend/dist/src/index.js");
+console.log("Loading app from:", filePath);
+require(filePath);
